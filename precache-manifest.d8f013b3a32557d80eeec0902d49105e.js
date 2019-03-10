c1563c36d124ca5c25d94440948e647ecce7dd46
karanche97 <karanche97@gmail.com>
2019-03-10 13:31:02 +0530

    Updates

---
+++ precache-manifest.d8f013b3a32557d80eeec0902d49105e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f1d7ff6e1ac1754356f",
+    "url": "/KaranChe97/snippt-task/static/js/runtime~main.1f1d7ff6.js"
+  },
+  {
+    "revision": "94f119d81481632faa86",
+    "url": "/KaranChe97/snippt-task/static/js/main.94f119d8.chunk.js"
+  },
+  {
+    "revision": "e44d08fc12f6f18b6a02",
+    "url": "/KaranChe97/snippt-task/static/js/2.e44d08fc.chunk.js"
+  },
+  {
+    "revision": "94f119d81481632faa86",
+    "url": "/KaranChe97/snippt-task/static/css/main.b5bf8cb2.chunk.css"
+  },
+  {
+    "revision": "e44d08fc12f6f18b6a02",
+    "url": "/KaranChe97/snippt-task/static/css/2.31274430.chunk.css"
+  },
+  {
+    "revision": "1b7bfc7b4a9eeaa1131125cf12dab3d9",
+    "url": "/KaranChe97/snippt-task/index.html"
+  }
+];
